d34e5e17c187630acd48569993ada685a6857945
Alexey Hellman <hellman1908@gmail.com>
2011-12-08 23:52:59 +0400

    updated

--- shtest.c
+++ shtest.c
@@ -4,33 +4,45 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <sys/mman.h>
-     
+
 /*------------------------------------------
     Shellcode testing program
     Usage:
-        shtest [-s sock_descriptor] {-f file | $'\xeb\xfe'}
+        shtest {-f file | $'\xeb\xfe' | '\xb8\x39\x05\x00\x00\xc3'}
     Usage example:
-        $ shtest $'\xeb\xfe'
-        $ shtest -f sh.bin
-        $ shtest -s 4 $'\xeb\xfe'
+        $ shtest $'\xeb\xfe'                 # raw shellcode
+        $ shtest '\xb8\x39\x05\x00\x00\xc3'  # escaped shellcode
+        $ shtest -f test.sc                  # shellcode from file
+        $ shtest -f <(python gen_payload.py) # test generated payload
     Compiling:
-        gcc shtest.c -o shtest && execstack -s shtest
-    Author: hellman
+        gcc -Wall shtest.c -o shtest
+    Author: hellman (hellman1908@gmail.com)
 -------------------------------------------*/
-
-//TODO: sock_descriptor
-//TODO: -p  - set handler in SIGPIPE or not
 
 char buf[4096];
 
 void usage() {
-    printf("Usage: shtest [-s sock_descriptor] {-f file | $'\\xeb\\xfe'}\n");
-    printf("\t-s: emulate a socket (via sock_descriptor)\n");
-    printf("\t-f: load shellcode from binaryfile\n");
+    printf("    Shellcode testing program\n\
+    Usage:\n\
+        shtest {-f file | $'\\xeb\\xfe' | '\\xb8\\x39\\x05\\x00\\x00\\xc3'}\n\
+    Usage example:\n\
+        $ shtest $'\\xeb\\xfe'                 # raw shellcode\n\
+        $ shtest '\\xb8\\x39\\x05\\x00\\x00\\xc3'  # escaped shellcode\n\
+        $ shtest -f test.sc                  # shellcode from file\n\
+        $ shtest -f <(python gen_payload.py) # test generated payload\n\
+    Compiling:\n\
+        gcc -Wall shtest.c -o shtest\n\
+    Author: hellman (hellman1908@gmail.com)\n");
+    exit(1);
+}
+
+void escape_error() {
+    printf("Shellcode is incorrectly escaped!\n");
     exit(1);
 }
 
 void run_shellcode(void *sc_ptr) {
+    int ret = 0;
     int (*ptr)();
     
     ptr = sc_ptr;
@@ -51,32 +63,28 @@ void run_shellcode(void *sc_ptr) {
     printf("Registers before call:\n");
     printf("  esp: %p, ebp: %p\n", esp, ebp);
     printf("  esi: %p, edi: %p\n", esi, edi);
-    
+
     printf("----------------------\n");
-    (*ptr)();
+    ret = (*ptr)();
     printf("----------------------\n");
     
-    printf("Shellcode returned (nice!).\n");
+    printf("Shellcode returned %d\n", ret);
     exit(0);
 }
 
 int main(int argc, char **argv) {
     
-    if (argc < 2)
+    if (argc < 2 || argc > 3)
         usage();
     
-    if (argc == 2 && (strlen(argv[1]) != 2 || argv[1][0] != '-'))
-        run_shellcode(argv[1]);
+    /*if (argc == 2 && (strlen(argv[1]) != 2 || argv[1][0] != '-'))
+        run_shellcode(argv[1]);*/
     
-    int sock = -1;
     char * fname = NULL;
     
     int c;
     while ((c = getopt (argc, argv, "hus:f:")) != -1) {
         switch (c) {
-            case 's':
-                sock = atoi(optarg);
-                break;
             case 'f':
                 fname = optarg;
                 break;
@@ -91,8 +99,26 @@ int main(int argc, char **argv) {
     if (optind < argc) {
         if (fname)
             usage();
-        else
-            run_shellcode(argv[optind]);
+
+        //try to translate from escapes ( \xc3 )
+        int i;
+        char *p1 = argv[optind];
+        char *p2 = argv[optind];
+        char *end = p1 + strlen(p1);
+
+        while (p1 < end) {
+            i = sscanf(p1, "\\x%02x", (unsigned int *)p2);
+            if (i != 1) {
+                if (p2 == p1) break;
+                else escape_error();
+            }
+
+            p1 += 4;
+            p2 += 1;
+        }
+
+        run_shellcode(argv[optind]);
+        return 0;
     }
     
     if (!fname)
